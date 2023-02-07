2beb0d763e220b24f63132da6cd6f015e3f7471b
갸갸 <quietwave00@gmail.com>
2023-02-07 19:15:23 +0900

    feat: Service 책 등록 test

--- src/main/java/site/metacoding/junitproject/web/dto/BookResponseDto.java
+++ src/main/java/site/metacoding/junitproject/web/dto/BookResponseDto.java
@@ -1,4 +1,21 @@
 package site.metacoding.junitproject.web.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.metacoding.junitproject.domain.Book;
+
+@NoArgsConstructor
+@Getter
 public class BookResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookResponseDto toDto(Book bookPS) {
+        this.id = bookPS.getId();
+        this.title = bookPS.getTitle();
+        this.author = bookPS.getAuthor();
+        return this;
+    }
 }
+
--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -1,4 +1,32 @@
 package site.metacoding.junitproject.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import site.metacoding.junitproject.domain.Book;
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.web.dto.BookResponseDto;
+import site.metacoding.junitproject.web.dto.BookSaveRequestDto;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor //final 생성자 주입
+@Service
 public class BookServiceTest {
+
+    private final BookRepository bookRepository;
+
+    //1. 책 등록
+    @Transactional(rollbackOn = Exception.class)
+    public BookResponseDto 책등록하기(BookSaveRequestDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookResponseDto().toDto(bookPS);
+    }
+
+    //2. 책 목록
+
+    //3. 책 단일 조회
+
+    //4. 책 삭제
+
+    //5. 책 수정
 }

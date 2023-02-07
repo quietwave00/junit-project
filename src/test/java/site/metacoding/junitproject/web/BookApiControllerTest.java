a426a70935b0a2a2fc4c70152c1e6b31d6be089d
갸갸 <quietwave00@gmail.com>
2023-02-07 14:38:45 +0900

    feat: Book entity

--- src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
+++ src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
@@ -1,4 +1,13 @@
 package site.metacoding.junitproject.web;
 
+import site.metacoding.junitproject.domain.Book;
+
 public class BookApiControllerTest {
+//    public void test() {
+//        Book book = Book.builder()
+//                .id(1L)
+//                .title("안녕")
+//                .author("안녕")
+//                .build();
+//    }
 }

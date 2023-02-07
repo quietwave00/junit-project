88a45782f19fab02e3bc6fd0acfb6a95cdab4b6a
갸갸 <quietwave00@gmail.com>
2023-02-07 15:36:30 +0900

    feat: 책 등록 test

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩(단위 테스트)
 public class BookRepositoryTest {
     //Controller(3)(클라이언트) - Service(2)(기능) - Repository(1)(DB)
@@ -14,8 +16,23 @@ public class BookRepositoryTest {
     // 1. 책 등록
     @Test
     public void 책등록_test() {
-        System.out.println("책등록_test 실행");
+        // given(데이터 준비)
+        String title = "junit5";
+        String author = "메타코딩";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        // when(테스트 실행)
+        Book bookPS = bookRepository.save(book); // DB에 저장, pk 생성
+                                                  // save 메서드가 DB에 저장된 Book을 return(DB와 동기화된 데이터 -> 영속화됨)
+
+        // then(검증)
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
     }
+
 
 
     // 2. 책 목록

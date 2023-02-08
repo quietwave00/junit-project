45cda2f85e588aad35aff70027af8d49caeba450
갸갸 <quietwave00@gmail.com>
2023-02-08 19:05:54 +0900

    feat: Service 책 등록 test

--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -2,22 +2,37 @@ package site.metacoding.junitproject.service;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.stereotype.Service;
 import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.util.MailSender;
 import site.metacoding.junitproject.util.MailSenderStub;
 import site.metacoding.junitproject.web.dto.BookResponseDto;
 import site.metacoding.junitproject.web.dto.BookSaveRequestDto;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
 
 
-@DataJpaTest
+@ExtendWith(MockitoExtension.class) //가짜 메모리 환경
 public class BookServiceTest {
 
-    @Autowired
+    @InjectMocks //@Mock 환경을 @InjectMocks를 통해 넣어줌
+    private BookService bookService;
+
+    @Mock
     private BookRepository bookRepository;
+
+    @Mock
+    private MailSender mailSender;
 
     @Test
     public void 책등록하기_테스트() {
@@ -26,16 +41,20 @@ public class BookServiceTest {
         dto.setTitle("junit강의");
         dto.setAuthor("메타코딩");
 
-        //stub
-        MailSenderStub mailSenderStub = new MailSenderStub();
+        //stub 행동 정의(=가설)
+        lenient().when(bookRepository.save(any())).thenReturn(dto.toEntity());
+        lenient().when(mailSender.send()).thenReturn(true);
 
         //when
-        BookService bookService = new BookService(bookRepository, mailSenderStub);
         BookResponseDto bookResponseDto = bookService.책등록하기(dto);
 
         //then
-        assertEquals(dto.getTitle(), bookResponseDto.getTitle());
-        assertEquals(dto.getAuthor(), bookResponseDto.getAuthor());
+//        assertEquals(dto.getTitle(), bookResponseDto.getTitle());
+//        assertEquals(dto.getAuthor(), bookResponseDto.getAuthor());
+        //asertj 사용
+        assertThat(dto.getTitle()).isEqualTo(bookResponseDto.getTitle());
+        assertThat(dto.getAuthor()).isEqualTo(bookResponseDto.getAuthor());
+
 
     }
 
